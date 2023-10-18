b4205f38653dc4cd48dc637cc75ba86832aca684
DamienToscano <toscano.damien@gmail.com>
2023-10-18 08:52:13 +0200

    feat(controls-loader): Add animation style on controls + Pause world if mobile

--- src/Experience/World/Controls.js
+++ src/Experience/World/Controls.js
@@ -33,32 +33,38 @@ export default class Controls extends EventEmitter
                     // this.camera.pan.reset()
                     this.actions.up = true
                     this.trigger('upStart')
+                    document.getElementById('control-up').classList.add('active')
                     break
 
                 case 'ArrowRight':
                     this.actions.right = true
                     this.trigger('rightStart')
+                    document.getElementById('control-right').classList.add('active')
                     break
 
                 case 'ArrowDown':
                     // this.camera.pan.reset()
                     this.actions.down = true
                     this.trigger('downStart')
+                    document.getElementById('control-down').classList.add('active')
                     break
 
                 case 'ArrowLeft':
                     this.actions.left = true
                     this.trigger('leftStart')
+                    document.getElementById('control-left').classList.add('active')
                     break
 
                 case 's':
                     this.actions.accelerate = true
                     this.trigger('accelerateStart')
+                    document.getElementById('control-accelerate').classList.add('active')
                     break
 
                 case 'd':
                     this.actions.fire = true
                     this.trigger('fireStart')
+                    document.getElementById('control-fire').classList.add('active')
                     break
 
             }
@@ -71,31 +77,37 @@ export default class Controls extends EventEmitter
                 case 'ArrowUp':
                     this.actions.up = false
                     this.trigger('upEnd')
+                    document.getElementById('control-up').classList.remove('active')
                     break
 
                 case 'ArrowRight':
                     this.actions.right = false
                     this.trigger('rightEnd')
+                    document.getElementById('control-right').classList.remove('active')
                     break
 
                 case 'ArrowDown':
                     this.actions.down = false
                     this.trigger('downEnd')
+                    document.getElementById('control-down').classList.remove('active')
                     break
 
                 case 'ArrowLeft':
                     this.actions.left = false
                     this.trigger('leftEnd')
+                    document.getElementById('control-left').classList.remove('active')
                     break
 
                 case 's':
                     this.actions.accelerate = false
                     this.trigger('accelerateEnd')
+                    document.getElementById('control-accelerate').classList.remove('active')
                     break
 
                 case 'd':
                     this.actions.fire = false
                     this.trigger('fireEnd')
+                    document.getElementById('control-fire').classList.remove('active')
                     break
             }
         }
